91b16d27a99da8f7f9a6d94460e3dd3d8bfae048
Ankur <ankur.rangi08@gmail.com>
2023-10-11 15:35:03 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('post/<int:pk>', views.PostDetailsView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('draft/', views.PostDraftView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name = 'add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name = 'comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name = 'comment_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name = 'post_publish'),
+    
+]
